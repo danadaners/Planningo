99eed416a620ab43d3ee5a10ddbb7125457fdb74
Dana Zhang <danadaners@gmail.com>
2021-03-07 18:53:22 -0500

    Fixing heroku bug

--- client/store/index.js
+++ client/store/index.js
@@ -9,28 +9,28 @@ import groups from "./allGroups";
 import singleGroup from "./singleGroup";
 import points from "./point";
 
-const loadState = () => {
-  try {
-    const serializedState = localStorage.getItem('state');
-    if(serializedState === null) {
-      return undefined;
-    }
-    return JSON.parse(serializedState);
-  } catch (e) {
-    return undefined;
-  }
-};
+// const loadState = () => {
+//   try {
+//     const serializedState = localStorage.getItem('state');
+//     if(serializedState === null) {
+//       return undefined;
+//     }
+//     return JSON.parse(serializedState);
+//   } catch (e) {
+//     return undefined;
+//   }
+// };
 
-const saveState = (state) => {
-  try {
-    const serializedState = JSON.stringify(state);
-    localStorage.setItem('state', serializedState);
-  } catch (e) {
-    // Ignore write errors;
-  }
-};
+// const saveState = (state) => {
+//   try {
+//     const serializedState = JSON.stringify(state);
+//     localStorage.setItem('state', serializedState);
+//   } catch (e) {
+//     // Ignore write errors;
+//   }
+// };
 
-const persistedState = loadState();
+// const persistedState = loadState();
 
 const reducer = combineReducers({
   user,
