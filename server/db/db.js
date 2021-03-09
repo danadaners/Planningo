78d68d44abd2c6b659413ec4d9816bf37e7260c0
Dana Zhang <danadaners@gmail.com>
2021-03-08 20:23:31 -0500

    heroku ssl error

--- server/db/db.js
+++ server/db/db.js
@@ -8,6 +8,10 @@ const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
     logging: false,
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
+
 );
 module.exports = db;
